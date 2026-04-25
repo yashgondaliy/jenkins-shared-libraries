0df73ef3bfc27a81aa9248ed9d2616f61337bd5d
Yash Gondaliya <135203186+yashgondaliy@users.noreply.github.com>
2026-04-25 21:27:11 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -6,7 +6,7 @@ def call(String credId,String imageName){
         )]){
         
         sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-        sh "docker image tag ${imageName} ${env.dockerHubUser}/two-tier-flask-app"
+        sh "docker image tag ${imageName} ${env.dockerHubUser}/${imageName}"
         sh "docker push ${env.dockerHubUser}/${imageName}:latest"
     }
     
